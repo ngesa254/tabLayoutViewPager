080b8aab4804ef2acb5971abfddfc8a1de2e2554
ngesa marvin <marvinngesa@gmail.com>
2018-02-12 07:18:46 +0300

    modify ViewerPager Adapter

--- app/src/main/java/io/droidmarvin/tablayoutviewpager/ViewPagerAdapter.java
+++ app/src/main/java/io/droidmarvin/tablayoutviewpager/ViewPagerAdapter.java
@@ -1,8 +1,57 @@
 package io.droidmarvin.tablayoutviewpager;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
 /**
  * Created by User on 2/12/2018.
  */
 
-public class ViewPagerAdapter {
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        if (position == 0)
+        {
+            fragment = new FragmentA();
+        }
+        else if (position == 1)
+        {
+            fragment = new FragmentB();
+        }
+        else if (position == 2)
+        {
+            fragment = new FragmentC();
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = null;
+        if (position == 0)
+        {
+            title = "Tab-1";
+        }
+        else if (position == 1)
+        {
+            title = "Tab-2";
+        }
+        else if (position == 2)
+        {
+            title = "Tab-3";
+        }
+        return title;
+    }
 }
